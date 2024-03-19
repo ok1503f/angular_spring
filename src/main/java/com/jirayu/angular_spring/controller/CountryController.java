883413b8cb3353d0279f44019ec4083e8bcc524f
Star Text <startext@Stars-MacBook-Air.local>
2024-03-19 23:12:28 +0700

    update

--- src/main/java/com/jirayu/angular_spring/controller/CountryController.java
+++ src/main/java/com/jirayu/angular_spring/controller/CountryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,9 @@ import com.jirayu.angular_spring.service.CountryService;
 @RestController
 @RequestMapping("/api/country")
 public class CountryController {
-    
+
     @Autowired
     CountryService countryService;
-    
 
     @RequestMapping("/findAll")
     @CrossOrigin(origins = "*")
@@ -28,7 +28,7 @@ public class CountryController {
 
     @RequestMapping("/save")
     @CrossOrigin(origins = "*")
-    public Country save(Country country) {
+    public Country save(@RequestBody Country country) {
         return countryService.save(country);
     }
 
@@ -47,14 +47,16 @@ public class CountryController {
         country.setName("Thailand");
         country.setArea(513120);
         country.setPopulation(70000000);
-        country.setFlags("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/510px-Flag_of_Thailand.svg.png");
+        country.setFlags(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/510px-Flag_of_Thailand.svg.png");
         countryList.add(country);
 
         Country country2 = new Country();
         country2.setName("japan");
         country2.setArea(513120);
         country2.setPopulation(70000000);
-        country2.setFlags("https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/250px-Flag_of_Japan.svg.png");
+        country2.setFlags(
+                "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/250px-Flag_of_Japan.svg.png");
         countryList.add(country2);
 
         countryService.saveAll(countryList);
